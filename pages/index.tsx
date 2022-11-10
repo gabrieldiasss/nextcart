0a635569e1cd339aaf3b4e183a0e26e98bc05e56
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2022-11-10 15:43:11 -0300

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,50 @@
 import { GlobalStyles } from "../styles/global";
 
-import { Container } from '../styles/app'
-import Header from "../components/Header";
-import Content from "../components/Content";
+import { Content, TitleContent } from '../styles/app'
+import { GetStaticProps } from "next";
+import Card from "../components/Card";
 
 export default function Home() {
   return (
-    <Container>
+    <>
 
       <GlobalStyles />
+      <Content>
+        <TitleContent>
+          <h1>
+            Pedidos
+          </h1>
 
-      <Header />
+          <span> | Últimos pedidos</span>
+        </TitleContent>
 
-      <Content />
-    </Container>
+        <Card />
+        <Card />
+        <Card />
+      </Content>
+    </>
   )
 }
+
+/* export const getStaticProps: GetStaticProps = async () => {
+
+  // categorys
+  const response = await fetch(`https://api.nytimes.com/svc/news/v3/content/section-list.json?q=everything&api-key=${process.env.API_KEY}`)
+  const data = await response.json()
+
+  // news
+  const responseNews = await fetch(`https://api.nytimes.com/svc/news/v3/content/nyt/all.json?q=everything&api-key=${process.env.API_KEY}`)
+  const dataNews = await responseNews.json()
+
+  console.log(data)
+  console.log(dataNews)
+
+  return {
+    props: {
+      category: data,
+      newsHome: dataNews
+    },
+
+    revalidate: 3600
+  }
+} */
--- styles/app.ts
+++ styles/app.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    
+export const Content = styled.main`
+    margin-top: 2rem;
+
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const TitleContent = styled.div`
+  display: flex;
+        align-items: center;
+
+
+        h1 {
+            font-family: Nunito Sans;
+            font-weight: 700;
+            color: #fff;
+        }
+
+        span {
+            font-family: Nunito Sans;
+            font-weight: 200;
+            color: #fff;
+            margin-left: 13px;
+        }
 `
